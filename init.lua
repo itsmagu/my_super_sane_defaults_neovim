c21f4da89891c8b83df233fe0ee39dbf31b5a1d5
magu <95685391+itsmagu@users.noreply.github.com>
2024-08-31 21:19:21 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- Bootstrap lazy.nvim
+-- Install Lazy if it's not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -23,43 +23,43 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-	  "nvim-treesitter/nvim-treesitter",
-	  {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
-},
-	  {
-  'ribru17/bamboo.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('bamboo').setup {
-      -- optional configuration here
-    }
-    require('bamboo').load()
-  end,
-},
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "bamboo" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	spec = {
+		"nvim-treesitter/nvim-treesitter",
+		{
+			"folke/which-key.nvim",
+			event = "VeryLazy",
+  			opts = {
+    		-- your configuration comes here
+    		-- or leave it empty to use the default settings
+    		-- refer to the configuration section below
+  			},
+  			keys = {
+    			{
+      				"<leader>?",
+      				function()
+        				require("which-key").show({ global = false })
+      				end,
+      			desc = "Buffer Local Keymaps (which-key)",
+    			},
+  			},
+		},
+	  	{
+  			'ribru17/bamboo.nvim',
+  			lazy = false,
+  			priority = 1000,
+  			config = function()
+    			require('bamboo').setup {
+      				-- optional configuration here
+    			}
+    			require('bamboo').load()
+  			end,
+		},
+	},
+  	-- Configure any other settings here. See the documentation for more details.
+  	-- colorscheme that will be used when installing plugins.
+  	install = { colorscheme = { "bamboo" } },
+  	-- automatically check for plugin updates
+  	checker = { enabled = true },
 })
 
 -- Sets
@@ -71,9 +71,19 @@ vim.opt.shiftwidth=4
 vim.opt.showbreak="..."
 vim.g.zig_fmt_parse_errors = 0
 vim.g.zig_fmt_autosave = 0
+
+if jit.os == 'Windows' then 
+	vim.opt.shell = "powershell"
+	vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+	vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+	vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+	vim.o.shellquote = ""
+	vim.o.shellxquote = ""
+end
+
 -- Keymaps
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = '\\'
 vim.keymap.set('n','<leader><F1>','<cmd>!odin run .<CR>')
 vim.keymap.set('n','.',"<nop>")
 vim.keymap.set("n", "Q", "<nop>")
